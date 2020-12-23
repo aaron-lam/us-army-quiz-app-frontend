7417cad8191be66fa89fdd2b2e574106caebe88b
aaronlam <aaronlam1618@gmail.com>
2020-12-23 11:47:26 -0800

    feat: setup ci/cd pipeline with gitHub actions

---
+++ release.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  branches: 'main',
+  repositoryUrl: 'https://github.com/aaron-lam/us-army-quiz-app-frontend',
+  plugins: [
+    '@semantic-release/commit-analyzer',
+    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/github',
+      {
+        assets: [
+          { path: 'build.zip', label: 'Build' },
+          { path: 'coverage.zip', label: 'Coverage' },
+        ],
+      },
+    ],
+  ],
+};
