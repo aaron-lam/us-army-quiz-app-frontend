7cfffa95c836f73dc498def2531a0ecf85566e3e
aaronlam <aaronlam1618@gmail.com>
2020-12-22 23:17:24 -0800

    first commit

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+  },
+  overrides: [
+    {
+      files: [
+        '*.test.js',
+        '*.test.jsx',
+      ],
+      env: {
+        jest: true,
+      },
+    },
+  ],
+};
