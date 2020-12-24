73832df4e68700b395e9a771015e12fb1328d081
aaronlam <aaronlam1618@gmail.com>
2020-12-23 21:19:11 -0800

    refactor: integrate typescript

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import * as React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World!/i);
+  const linkElement = screen.getByText(/Good Morning!/i);
   expect(linkElement).toBeInTheDocument();
 });
