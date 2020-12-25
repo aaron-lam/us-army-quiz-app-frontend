ec0df75e255139e85e177c30838e8536dc6bb96a
aaronlam <aaronlam1618@gmail.com>
2020-12-25 12:41:05 -0800

    fix: remove class name borderless from app header segment

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -3,7 +3,7 @@ import { Header, Segment } from 'semantic-ui-react';
 import { APP_HEADER, ARMY_LOGO_URL } from '../contants';
 
 const AppHeader: React.FC = (): ReactElement => (
-  <Segment inverted borderless attached="bottom">
+  <Segment inverted attached="bottom">
     <Header as="h2" textAlign="center" image={ARMY_LOGO_URL} content={APP_HEADER} />
   </Segment>
 );
