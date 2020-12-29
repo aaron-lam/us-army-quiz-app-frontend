7cde78b93652ad3dce246b070debacb1458b7561
aaronlam <aaronlam1618@gmail.com>
2020-12-28 20:05:38 -0800

    fix: fix react router duplicated bug

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,15 @@ import { ToastContainer } from 'react-toastify';
 import App from './app';
 import 'react-toastify/dist/ReactToastify.css';
 import 'semantic-ui-less/semantic.less';
+import {
+  LOCAL_STORAGE_LAST_NAME_KEY,
+  LOCAL_STORAGE_UNIT_ID_KEY,
+  LOCAL_STORAGE_UNIT_KEY,
+} from './contants';
+
+localStorage.setItem(LOCAL_STORAGE_LAST_NAME_KEY, '');
+localStorage.setItem(LOCAL_STORAGE_UNIT_ID_KEY, '');
+localStorage.setItem(LOCAL_STORAGE_UNIT_KEY, '');
 
 ReactDOM.render(
   <React.StrictMode>
