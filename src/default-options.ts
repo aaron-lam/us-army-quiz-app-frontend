e986bf549b8cbb7f3d25972674bcf26b1b9c5aaf
aaronlam <aaronlam1618@gmail.com>
2021-01-02 14:23:15 -0800

    feat: make get, put, delete api calls when users interact on question builder page

---
+++ src/default-options.ts
@@ -0,0 +1,10 @@
+// defaultOptions.js
+const defaultOptions = {
+  headers: {
+    Accept: 'application/json',
+    Authorization: 'Bearer ArmyQuizApp!',
+    'Content-Type': 'application/json',
+  },
+};
+
+export default defaultOptions;
