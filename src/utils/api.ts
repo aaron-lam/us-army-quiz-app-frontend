4c28d7ac179638a10a9b5440ba987c304d6e993f
aaronlam <aaronlam1618@gmail.com>
2021-01-03 13:55:35 -0800

    fix: update is response not success definition

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,3 +1,3 @@
-const isNotSuccess = (statusCode: number): boolean => statusCode / 100 !== 2;
+const isNotSuccess = (statusCode: number): boolean => statusCode / 100 === 4 || statusCode / 100 === 5;
 
 export default isNotSuccess;
